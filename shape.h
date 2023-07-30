bfc0f8fba117606ffe225fbb316fd4ab75a7b057
GarDeN <garpinich22@mail.ru>
2023-07-30 17:46:50 +0300

    added enum for figures

--- shape.h
+++ shape.h
@@ -1,17 +1,23 @@
 #pragma once
 #include<string>
+
+
+
 class Shape
 {
 public:
-	static const int line = 0;
-	static const int sqr = 1;
-	static const int cube = 2;
-	static const int circle = 3;
-	static const int cylinder = 4;
 	Shape() = default;
 	Shape(int type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8);
 	Shape(int type, int _x1, int _y1, double R, double H);
 	int getType() { return type; }
+	static enum figure
+	{
+		line,
+		sqr,
+		cube,
+		circle,
+		cylinder
+	};
 
 	int type;
 	int x1 = 0, y1 = 0, z1 = 0, 
