9389cba13d5e23b5dbfc108c0123a26e74d3dbed
GarDeN <garpinich22@mail.ru>
2023-08-04 14:08:55 +0300

    correct HW

--- shape.h
+++ shape.h
@@ -1,46 +1,60 @@
 #pragma once
-#include<string>
+#define _USE_MATH_DEFINES
+#include<cmath>
 
+enum class figures
+{
+	line,
+	sqr,
+	cube,
+	circle,
+	cylinder
+};
 
-
-class Shape
+class Shape1
 {
 public:
-	Shape() = default;
-	Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8);
-	Shape(int _type, int _x1, int _y1, double R, double H);
+	Shape1() = default;
 	int getType() { return type; }
-	static enum class figures
-	{
-		line,
-		sqr,
-		cube,
-		circle,
-		cylinder
-	};
 
+	virtual void shift(int m, int n, int k) = 0;
+	virtual void scaleX(int _a) = 0;
+	virtual void scaleY(int d) = 0;
+	virtual void scaleZ(int e) = 0;
+	virtual void scale(int s) = 0;
+
+protected:
 	int type;
-	int x1 = 0, y1 = 0, z1 = 0, 
-		x2 = 0, y2 = 0, z2 = 0, 
-		x3 = 0, y3 = 0, z3 = 0, 
-		x4 = 0, y4 = 0, z4 = 0, 
-		x5 = 0, y5 = 0, z5 = 0, 
-		x6 = 0, y6 = 0, z6 = 0, 
-		x7 = 0, y7 = 0, z7 = 0, 
+	int a = 0, b = 0, c = 0;
+	double volume = 0;
+	double square = 0;
+	int x1 = 0, y1 = 0, z1 = 0,
+		x2 = 0, y2 = 0, z2 = 0,
+		x3 = 0, y3 = 0, z3 = 0,
+		x4 = 0, y4 = 0, z4 = 0,
+		x5 = 0, y5 = 0, z5 = 0,
+		x6 = 0, y6 = 0, z6 = 0,
+		x7 = 0, y7 = 0, z7 = 0,
 		x8 = 0, y8 = 0, z8 = 0;
-	double volume;
-	double square;
-	double height;
-	double radius;
-	void fillingCoordinates(const int& _x1, const int& _y1, const int& _z1, const int& _x2, const int& _y2, const int& _z2, const int& _x3, const int& _y3, const int& _z3, const int& _x4, const int& _y4, const int& _z4, const int& _x5, const int& _y5, const int& _z5, const int& _x6, const int& _y6, const int& _z6, const int& _x7, const int& _y7, const int& _z7, const int& _x8, const int& _y8, const int& _z8);
-	void fillingCoordinates(const int& _x1, const int& _y1, const double& R, const double& H);
+	virtual void calculateSquare(const int& a, const int& b, const int& c) = 0;
 
-	void calculateSquare(const int& a, const int& b, const int& c);
-	void calculateSquare(const double& R, const double& H);
+	virtual void calculateVolume(const int& a, const int& b, const int& c) = 0;
+};
 
-	void calculateVolume(const int& a, const int& b, const int& c);
-	void calculateVolume(const double& R, const double& H);
+class Shape2
+{
+public:
+	Shape2() = default;
+	int getType() { return type; }
 
+protected:
+	int type;
+	double volume = 0;
+	double square = 0;
+	double height = 0;
+	double radius = 0;
+	int x1 = 0, y1 = 0;
+	virtual void calculateSquare(const double& R, const double& H) = 0;
 
-
+	virtual void calculateVolume(const double& R, const double& H) = 0;
 };
